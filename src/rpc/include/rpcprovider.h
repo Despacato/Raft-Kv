4825e73d2b56c5d3ed6ae30bb1f9665cb4c1b520
z <dlmu_zxg@163.com>
2025-04-16 00:00:04 +0800

    <fix>(src): Rpcprovider 发布服务方法

--- src/rpc/include/rpcprovider.h
+++ src/rpc/include/rpcprovider.h
@@ -4,6 +4,8 @@
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/TcpServer.h>
 #include <muduo/net/InetAddress.h>
+#include <string>
+#include <unordered_map>
 
 class RpcProvider
 {
@@ -17,7 +19,15 @@ private:
     // std::unique_ptr<muduo::net::TcpServer> m_tcpServerPtr; // 网络服务对象
     muduo::net::EventLoop m_eventLoop; // 事件循环对象
 
+    // service服务类型信息
+    struct ServiceInfo
+    {
+        google::protobuf::Service *m_service;                                                    // 服务对象
+        std::unordered_map<std::string, const google::protobuf::MethodDescriptor *> m_methodMap; // 方法描述信息
+    };
+    // 存储注册成功的服务对象和其服务方法的所有信息
+    std::unordered_map<std::string, ServiceInfo> m_serviceMap;
     // 新的socket连接回调
-    void OnConnection(const muduo::net::TcpConnectionPtr &conn); 
+    void OnConnection(const muduo::net::TcpConnectionPtr &conn);
     void OnMessage(const muduo::net::TcpConnectionPtr &conn, muduo::net::Buffer *buffer, muduo::Timestamp time);
 };
--- src/rpc/rpcprovider.cpp
+++ src/rpc/rpcprovider.cpp
@@ -1,10 +1,30 @@
 #include "rpcprovider.h"
 #include "mprpcapplication.h"
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/service.h>
 #include <string>
 
 void RpcProvider::NotifyService(google::protobuf::Service *service)
 {
+    ServiceInfo service_info;
+    //获取服务对象的描述信息
+    const google::protobuf::ServiceDescriptor *desc = service->GetDescriptor();
+    //获取服务名称
+    std::string service_name = desc->name();
+    //获取服务方法数量
+    int method_count = desc->method_count();
+    //获取服务方法
+    for (int i = 0; i < method_count; ++i)
+    {
+        //获取方法描述信息
+        const google::protobuf::MethodDescriptor *method = desc->method(i);
+        std::string method_name = method->name();
+        service_info.m_methodMap.insert({method_name, method});
+       
+    }
+    service_info.m_service = service;
 
+    m_serviceMap.insert({service_name, service_info});
 }
 
 void RpcProvider::Run()
